c8cab74f5f618490cb0b34c4bfac7471c815eb5b
Audrey Clark <amazon.audrey@gmail.com>
2021-08-16 14:00:30 -0700

    first commit

---
+++ wood-backend/src/main/java/com/woodapp/woodbackend/WoodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.woodapp.woodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodBackendApplication.class, args);
+	}
+
+}
---
+++ wood-backend/src/test/java/com/woodapp/woodbackend/WoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodapp.woodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
