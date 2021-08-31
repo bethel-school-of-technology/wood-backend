ffc98d1113b465b0010d1ca107bc14404ff0a7e7
Audrey Clark <amazon.audrey@gmail.com>
2021-08-31 00:33:56 -0700

    Updated project for user registration and login

--- src/main/java/com/woodapp/message_board/MessageController.java
+++ src/main/java/com/woodapp/message_board/MessageController.java
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RestController
 public class MessageController {
-	
-	@Autowired
+
 	MessageRepository dao;
 	
 	@GetMapping("/message-board")
--- src/main/java/com/woodapp/users/Users.java
+++ src/main/java/com/woodapp/users/Users.java
@@ -1,99 +1,82 @@
 package com.woodapp.users;
 
+import com.woodapp.registration.AppUserRole;
+import lombok.EqualsAndHashCode;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+@EqualsAndHashCode
 @Entity(name="Users")
 @Table(name="users")
 
-public class Users {
+public class Users implements UserDetails{
 
 	@Id
-	@SequenceGenerator(
-			name = "user_sequence",
-			sequenceName = "user_sequence",
-			allocationSize = 1
-	)
-	@GeneratedValue(
-			strategy = GenerationType.SEQUENCE,
-			generator = "user_sequence"
-	)
-	@Column(
-            name = "id",
-            updatable = false
-    )
+	@SequenceGenerator(name="user_sequence", sequenceName="user_sequence", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="user_sequence")
+
+	@Column(name="id", updatable=false)
 	private Integer id;
 
 	@NotNull
 	@Size(min=2, max=30)
-	@Column(
-            name="first_name",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
+	@Column(name="first_name", nullable=false, columnDefinition="TEXT")
 	private String firstName;
 
 	@NotNull
 	@Size(min=2, max=30)
-	@Column(
-            name="last_name",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
+	@Column(name="last_name", nullable=false, columnDefinition="TEXT")
 	private String lastName;
 
 	@NotNull
 	@Email
-	@Column(
-            name="email",
-            nullable = false,
-            columnDefinition = "TEXT",
-			unique = true
-    )
+	@Column(name="email", nullable=false, columnDefinition="TEXT", unique = true)
 	private String email;
+
+	private String username;
 
 	@NotNull
 	@Size(min=6, max=20)
-	@Column(
-            name="password",
-            nullable = false,
-            unique = true
-    )
+	@Column(name="password", nullable=false, unique=true)
 	private String password;
 
 	private String gender;
 	private Integer birthday;
 
 	@NotNull
-	@Column(
-            name="phone_number",
-            nullable = false
-    )
+	@Column(name="phone_number", nullable=false)
 	private Integer phoneNumber;
 
 	@NotNull
-	@Column(
-            name="street_address",
-            nullable = false
-    )
+	@Column(name="street_address", nullable=false)
     private String streetAddress;
 	private String state;
 	private Integer zipCode;
 	private LocalDate signUpDate = LocalDate.now();
+	@Enumerated(EnumType.STRING)
+	private AppUserRole appUserRole;
+	private Boolean locked;
+	private Boolean enabled;
 
 	public Users() {
 	}
 
-	public Users(String firstName, String lastName, String email, String password, String gender, Integer birthday,
-				 Integer phoneNumber, String streetAddress, String state, Integer zipCode, LocalDate signUpDate) {
+	public Users(String firstName, String lastName, String email, String username, String password, String gender, Integer birthday,
+				 Integer phoneNumber, String streetAddress, String state, Integer zipCode, LocalDate signUpDate,
+				 AppUserRole appUserRole, Boolean locked, Boolean enabled) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
+		this.username = username;
 		this.password = password;
 		this.gender = gender;
 		this.birthday = birthday;
@@ -102,14 +85,15 @@ public class Users {
 		this.state = state;
 		this.zipCode = zipCode;
 		this.signUpDate = signUpDate;
+		this.appUserRole = appUserRole;
+		this.locked = locked;
+		this.enabled = enabled;
 	}
 
 	public Integer getId() {
 		return id;
 	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
+	public void setId(Integer id) {this.id = id;}
 	public String getFirstName() {
 		return firstName;
 	}
@@ -128,9 +112,9 @@ public class Users {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getPassword() {
-		return password;
-	}
+//	public String getUsername(){ return username;}
+	public void setUsername(String username){this.username = username;}
+//	public String getPassword() { return password;}
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -193,5 +177,34 @@ public class Users {
 				", zipCode=" + zipCode +
 				", signUpDate=" + signUpDate +
 				'}';
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+		return Collections.singletonList(authority);
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+	@Override
+	public String getUsername() { return username;}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return !locked;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return enabled;
 	}
 }
